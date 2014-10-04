2a08c4484ab214276e32385539a0db1b88739d27
Stefan Penner <stefan.penner@gmail.com>
2014-10-03 23:33:42 -0400

    weboutput

--- bench.js
+++ bench.js
@@ -1,18 +1,34 @@
 var Benchmark = require('benchmark');
 
+function log(message) {
+  if (window) {
+    var div = document.createElement('div');
+    div.textContent = message;
+    document.body.appendChild(div);
+  } else  {
+    console.log(message);
+  }
+}
+
 module.exports = function(suites) {
   var suite = new Benchmark.Suite();
 
+  log('testing');
   suites.forEach(function(s) {
+    log('- ' + s.name);
     suite.add(s.name, s.fn);
   });
 
   suite.on('cycle', function(event) {
-    console.log(String(event.target));
+    log(String(event.target));
   })
   .on('complete', function() {
-    console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+    log('Fastest is ' + this.filter('fastest').pluck('name'));
   });
 
-  suite.run({ 'async': true });
+  setTimeout(function() {
+    suite.run({
+      'async': true
+    });
+  }, 1000);
 }
