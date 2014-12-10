07ee198cc8ba6a6d333a36b1bfe91347399a49c3
Stefan Penner <stefan.penner@gmail.com>
2014-12-10 10:29:25 -0500

    fix build + add pygmentize

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ echo "building $1"
 
 mkdir $1/
 cp index.html $1/index.html
-echo "<pre>$(cat $1.js) </pre>" >> $1/index.html
+echo "<pre>$(cat $1.js | pygmentize -l javascript -f html) </pre>" >> $1/index.html
+echo "<style>$(pygmentize -f html -S colorful)</style>" >> $1/index.html
 browserify $1.js -o $1/out.js
-ssh og "mkdir ~/htdocs/$1"
-scp -r $1 og:~/htdocs/
 
