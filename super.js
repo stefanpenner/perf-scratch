965070a1852f7d7daeededa92f3beb1770e5c8f8
Stefan Penner <stefan.penner@gmail.com>
2014-12-14 19:25:57 -0500

    more super stuff

--- super.js
+++ super.js
@@ -11,6 +11,10 @@ class Bar {
   traditionalName() {
     return 'BAR';
   }
+
+  emberName() {
+    return 'BAR';
+  }
 }
 
 class Foo extends Bar {
@@ -23,6 +27,10 @@ class Foo extends Bar {
 
   traditionalName() {
     return 'FOO' + Bar.prototype.traditionalName.call(this);
+  }
+
+  emberName() {
+    return 'FOO' + this._super();
   }
 }
 
@@ -38,8 +46,47 @@ class Baz extends Foo {
   traditionalName() {
     return 'Baz' + Foo.prototype.traditionalName.call(this);
   }
+
+  emberName() {
+    return 'Baz' + this._super();
+  }
 }
 
+function giveMethodSuper(_super, fn) {
+  return function() {
+    var previousSuper = this._super;
+    this._super = _super;
+
+    var result = fn.apply(this, arguments);
+    this._super = previousSuper;
+    return result;
+  }
+};
+
+Bar.prototype.emberName = function() {
+  return 'BAR';
+};
+
+Foo.prototype.emberName = giveMethodSuper(Bar.prototype.emberName, Foo.prototype.emberName);
+Baz.prototype.emberName = giveMethodSuper(Foo.prototype.emberName, Baz.prototype.emberName);
+
+Bar.prototype.toMethodName = function() {
+  return 'BAR';
+};
+
+Foo.prototype.constructor = Foo;
+ 
+Foo.prototype.toMethodName = function () {
+    return 'FOO' + super.name();
+}.toMethod(Foo.prototype);
+
+Baz.prototype.constructor = Baz;
+ 
+Baz.prototype.toMethodName = function () {
+    return 'Baz' + super.name();
+}.toMethod(Baz.prototype);
+ 
+ 
 Foo.prototype._superBar$otherName = Bar.prototype.otherName;
 Baz.prototype._superFoo$otherName = Foo.prototype.otherName;
 
@@ -61,6 +108,14 @@ assert(bar.traditionalName() === 'BAR');
 assert(foo.traditionalName() === 'FOOBAR');
 assert(baz.traditionalName() === 'BazFOOBAR');
 
+assert(bar.toMethodName() === 'BAR');
+assert(foo.toMethodName() === 'FOOBAR');
+assert(baz.toMethodName() === 'BazFOOBAR');
+
+assert(bar.name() === 'BAR');
+assert(foo.emberName() === 'FOOBAR');
+assert(baz.emberName() === 'BazFOOBAR');
+
 require('./bench')([
   { name: 'bar.name', fn: function() { return bar.name(); } },
   { name: 'foo.name', fn: function() { return foo.name(); } },
@@ -73,4 +128,12 @@ require('./bench')([
   { name: 'bar.traditionalName', fn: function() { return bar.traditionalName(); } },
   { name: 'foo.traditionalName', fn: function() { return foo.traditionalName(); } },
   { name: 'baz.traditionalName', fn: function() { return baz.traditionalName(); } },
-]);
+
+  { name: 'bar.toMethodName', fn: function() { return bar.toMethodName(); } },
+  { name: 'foo.toMethodName', fn: function() { return foo.toMethodName(); } },
+  { name: 'baz.toMethodName', fn: function() { return baz.toMethodName(); } },
+
+  { name: 'bar.emberName', fn: function() { return bar.emberName(); } },
+  { name: 'foo.emberName', fn: function() { return foo.emberName(); } },
+  { name: 'baz.emberName', fn: function() { return baz.emberName(); } },
+])
