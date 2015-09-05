9d41be12a4205b536091f7861f75b673d661db79
Stefan Penner <stefan.penner@gmail.com>
2015-09-05 08:27:12 -0700

    testing - new F(x,y) - new F(pojo) - struct (seal) - struct (no-seal) - struct2 (seal) - struct2 (no-seal) - struct3 (seal) - struct3 (no-seal) new F(x,y) x 71,477,516 ops/sec ±1.33% (80 runs sampled) new F(pojo) x 74,383,455 ops/sec ±1.19% (87 runs sampled) struct (seal) x 1,779,045 ops/sec ±1.41% (87 runs sampled) struct (no-seal) x 2,324,039 ops/sec ±1.21% (91 runs sampled) struct2 (seal) x 3,350,765 ops/sec ±1.29% (84 runs sampled) struct2 (no-seal) x 5,595,888 ops/sec ±1.67% (77 runs sampled) struct3 (seal) x 6,269,766 ops/sec ±0.89% (96 runs sampled) struct3 (no-seal) x 19,701,202 ops/sec ±0.87% (94 runs sampled) Fastest is new F(pojo)

--- create.js
+++ create.js
@@ -1,13 +1,165 @@
-function Eric() {
-
+function One(foo, bar) {
+  this.foo = foo
+  this.bar = bar;
 }
 
-var Kris = {};
-Eric.prototype = Kris;
+function Two(options) {
+  this.foo = options.foo;
+  this.bar = options.bar;
+}
+
+var shape = {
+  foo: undefined,
+  bar: undefined
+};
+
+var keys = Object.keys(shape);
+
+function REQUIRED() { }
+
+function StructSeal(options){
+  var c = this;
+  keys.forEach(function(key) {
+    if (options.hasOwnProperty(key)) {
+      c[key] = options[key];
+    } else if (shape[key] === REQUIRED) {
+      throw new Error("Missing required" + key + " in struct");
+    } else {
+      c[key] = shape[field];
+    }
+  });
+
+  Object.seal(this);
+}
+
+function Struct(options){
+  var c = this;
+  keys.forEach(function(key) {
+    if (options.hasOwnProperty(key)) {
+      c[key] = options[key];
+    } else if (shape[key] === REQUIRED) {
+      throw new Error("Missing required" + key + " in struct");
+    } else {
+      c[key] = shape[key];
+    }
+  });
+}
+
+function Struct2Seal(options){
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    if (options.hasOwnProperty(key)) {
+      this[key] = options[key];
+    } else if (shape[key] === REQUIRED) {
+      throw new Error("Missing required" + key + " in struct");
+    } else {
+      this[key] = shape[key];
+    }
+  }
+
+  Object.seal(this);
+}
+
+function Struct2(options){
+  for (var i = 0; i< keys.length; i++) {
+    var key = keys[i];
+    if (options.hasOwnProperty(key)) {
+      this[key] = options[key];
+    } else if (shape[key] === REQUIRED) {
+      throw new Error("Missing required" + key + " in struct");
+    } else {
+      this[key] = shape[key];
+    }
+  }
+}
+
+function Struct3(options){
+  for (var i = 0; i< keys.length; i++) {
+    var key = keys[i];
+    this[key] = options[key];
+  }
+}
+
+function Struct3Seal(options){
+  for (var i = 0; i< keys.length; i++) {
+    var key = keys[i];
+    this[key] = options[key];
+  }
+
+  Object.seal(this);
+}
+
+function testOne() {
+  return new One('a', 'b');
+}
+
+function testTwo() {
+  return new Two({
+    foo: 'a',
+    bar: 'b'
+  });
+}
+
+
+function testStruct() {
+  return new Struct({
+    foo: 'a',
+    bar: 'b'
+  });
+}
+
+function testStructSeal() {
+  return new StructSeal({
+    foo: 'a',
+    bar: 'b'
+  });
+}
+
+function testStruct2Seal() {
+  return new Struct2Seal({
+    foo: 'a',
+    bar: 'b'
+  });
+}
+
+function testStruct2() {
+  return new Struct2({
+    foo: 'a',
+    bar: 'b'
+  });
+}
+
+function testStruct3() {
+  return new Struct3({
+    foo: 'a',
+    bar: 'b'
+  });
+}
+
+function testStruct3Seal() {
+  return new Struct3Seal({
+    foo: 'a',
+    bar: 'b'
+  });
+}
+
+// run the scenarios to ensure they work, if they returned something complex assert for it
+testOne();
+testTwo();
+testStructSeal();
+testStruct();
+testStruct2Seal();
+testStruct2();
 
 require('./bench')([
-  { name: 'new',      fn: function() { return new Eric(); } },
-  { name: 'o create', fn: function() { return Object.create(Kris); } },
+  { name: 'new F(x,y)',          fn: testOne         },
+  { name: 'new F(pojo)',         fn: testTwo         },
+  { name: 'struct (seal)',       fn: testStructSeal  },
+  { name: 'struct (no-seal)',    fn: testStruct      },
+  { name: 'struct2 (seal)',      fn: testStruct2Seal },
+  { name: 'struct2 (no-seal)',   fn: testStruct2     },
+  { name: 'struct3 (seal)',      fn: testStruct3Seal },
+  { name: 'struct3 (no-seal)',   fn: testStruct3     }
 ]);
 
 
