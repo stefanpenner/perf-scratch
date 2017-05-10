c18a61df8c48a2e0447dc8c6fb813b07d969e996
Stefan Penner <stefan.penner@gmail.com>
2017-05-10 08:57:01 -0700

    Update index-of-vs-char-at.js

--- index-of-vs-char-at.js
+++ index-of-vs-char-at.js
@@ -15,3 +15,27 @@ require('do-you-even-bench')([
 ]);
 
 
+// testing
+// - charCodeAt
+// - indexOf
+// running first test, please wait...
+//  charCodeAt ... 48,437,997.64 op/s
+//  indexOf ...... 17,884,808.89 op/s
+
+/*
+npm version
+{ 'perf-scratch': '1.0.0',
+  npm: '4.0.5',
+  ares: '1.10.1-DEV',
+  cldr: '30.0.3',
+  http_parser: '2.7.0',
+  icu: '58.2',
+  modules: '51',
+  node: '7.4.0',
+  openssl: '1.0.2j',
+  tz: '2016j',
+  unicode: '9.0',
+  uv: '1.10.1',
+  v8: '5.4.500.45',
+  zlib: '1.2.8' }
+*/
