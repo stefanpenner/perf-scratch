9a2d87d2035c65be143a60d88cb9faa03f60488f
Stefan Penner <stefan.penner@gmail.com>
2019-01-31 14:12:34 -0800

    See if worker-threads can share the cost of requiring with the parent, looks like no.

---
+++ worker_thread/index.js
@@ -0,0 +1,27 @@
+/*
+ *  node-set v11                                                                                                                             (573ms)
+ *   ✓ node.current = v11.9.0
+ *  s/s/perf-scratch/worker_thread ╍{} node index.js
+ * main: babel-core: 194.280ms
+ * thread: babel-core: 224.869ms
+ * thread: babel-core: 223.196ms
+ * thread: babel-core: 222.100ms
+ */
+const {
+  Worker, isMainThread, parentPort, workerData
+} = require('worker_threads');
+
+if (isMainThread) {
+  console.time('main: babel-core');
+  require('babel-core');
+  console.timeEnd('main: babel-core');
+  for (let i = 0; i < 3; i++) {
+    new Worker(__filename, {
+      workerData: __filename
+    });
+  }
+} else {
+  console.time('thread: babel-core');
+  require('babel-core');
+  console.timeEnd('thread: babel-core');
+}
